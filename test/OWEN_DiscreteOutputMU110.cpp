4c514318c965ea8233dd6decac21249f5fef2a79
Alexander Ismailov <alexismailov2@gmail.com>
2020-09-10 18:11:49 +0500

    Added parameter for setting serial port to test.
    Signed-off-by: Alexander Ismailov <alexismailov2@gmail.com>

--- test/OWEN_DiscreteOutputMU110.cpp
+++ test/OWEN_DiscreteOutputMU110.cpp
@@ -5,7 +5,7 @@
 
 auto main(int argc, char** argv) -> int32_t
 {
-   auto discreteOutput = OWEN::DiscreteOutputMU110{"/dev/cu.usbserial-14510", 115200, 17};
+   auto discreteOutput = OWEN::DiscreteOutputMU110{argv[1], 115200, 17};
 //   auto const communicationOptions = discreteOutput.GetCommunicationOptions();
 //   if (communicationOptions.has_value())
 //   {
@@ -25,9 +25,10 @@ auto main(int argc, char** argv) -> int32_t
 //   {
 //     std::cout << "error" << std::endl;
 //   }
-   auto const discreteOutputState = discreteOutput.GetDiscreteOutputsState();
    for (auto i = 0; i < 255; ++i)
    {
+     auto const discreteOutputState = discreteOutput.GetDiscreteOutputsState();
+     std::cout << "discreteoutputState: " << std::hex << static_cast<uint32_t>(discreteOutputState.value()) << std::endl;
      std::this_thread::sleep_for(std::chrono::seconds(1));
      discreteOutput.SetDiscreteOutputsState(i);
    }
